e99eea5660142befdf8fe05aa9e86cc71f35a48d
Aryam <165002841+Aryamatallah@users.noreply.github.com>
2026-02-09 16:05:44 +0300

    WID & WATCH

---
+++ SteepishOSWatch Watch App/SteepishOSWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteepishOSWatchApp.swift
+//  SteepishOSWatch Watch App
+//
+//  Created by Aryam on 09/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct SteepishOSWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SteepishOSWatch/SteepishOSWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteepishOSWatchApp.swift
+//  SteepishOSWatch
+//
+//  Created by Aryam on 09/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct SteepishOSWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
